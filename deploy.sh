5ba0f32642c71b32b514b7411f99cf02c489484f
Jonathan Sterling <jonsterling@me.com>
2013-11-09 16:28:00 -0800

    deployed site

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git checkout master
 git pull origin master
 find . -maxdepth 1 -not -name '.git' | xargs rm -rf
 cp -rf ../_site/* .
-git add .
+git add -a .
 git commit -m "deploy"
 git push origin master
 cd ..
